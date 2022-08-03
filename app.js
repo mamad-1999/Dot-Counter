225b0889fede9a5866f843d35a14525f10c9c95a
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2022-08-03 17:32:24 +0430

    modified: add check arabic word function

--- app.js
+++ app.js
@@ -13,17 +13,31 @@ const errText = document.querySelector('.err-text')
 let wordsArray = [
     ['ا', 'ص', 'ع', 'ه', 'ح', 'گ', 'ک', 'م', 'آ', 'ل', 'س', 'و', 'ط', 'د', 'ر'],
     ['ب', 'ج', 'خ', 'ذ', 'ز', 'ظ', 'ض', 'ف', 'غ', 'ن'],
-    ['ت', 'ق', 'ی','ة','ي'],
+    ['ت', 'ق', 'ی'],
     ['پ', 'ث', 'چ', 'ژ', 'ش'],
 ];
 
-const checkDot = (word) => {
-    let dotCount = 0;
-    const pattern = /[ی]\s|[ی]$/g;
-    //از این الگو برای پیدا کردن ی های آخر که نقطه ندارند استفاده شده
-    //هم ی های آخر جمله و هم وسط جمله که بعد آنها فاصله هست شناسایی شده در یک آرایه قرار میگیرند
+const pattern = /[ی]\s|[ی]$/g;
+//از این الگو برای پیدا کردن ی های آخر که نقطه ندارند استفاده شده
+//هم ی های آخر جمله و هم وسط جمله که بعد آنها فاصله هست شناسایی شده در یک آرایه قرار میگیرند
+const arabicPattern = /(ي|ة)/g;
+// از این الگو برای شناسایی کلمات عربی استفاده شده
 
+const checkDotHandler = (word) => {
+    let dotCount = 0;
     const strigWord = word.match(pattern);
+    const arabicWord = word.match(arabicPattern)
+
+    let dotCounter = counterDot(word, dotCount);
+    let dotDepleCounter = checkDepoleString(strigWord, dotCounter)
+    let dotArabicCounter = arabicWordCheck(arabicWord, dotDepleCounter)
+
+    countBox.classList.add('active')
+    errText.classList.remove('active')
+    countdotText.innerHTML = dotArabicCounter
+}
+
+const counterDot = (word, dotCount) => {
     const chars = word.split('');
 
     for (let i = 0; i < wordsArray.length; i++) {
@@ -37,22 +51,32 @@ const checkDot = (word) => {
         });
     };
 
-    if (strigWord) {
-        const minesCount = strigWord.length * 2;
-        dotCount -= minesCount;
+    return dotCount
+}
+
+const checkDepoleString = (stringGroupe, dotCount) => {
+    if (stringGroupe) {
+        const minesCount = stringGroupe.length * 2;
+        return dotCount -= minesCount;
         //تعداد ی های بدون نقطه در ضریب تعداد نقطه که ۲ میباشد از تعداد کل نقطه ها کم میشود 
     };
+    return dotCount
+}
 
-    countBox.classList.add('active')
-    errText.classList.remove('active')
-    countdotText.innerHTML = dotCount
+const arabicWordCheck = (stringGroupe, dotCount) => {
+    if (stringGroupe) {
+        const plusCount = stringGroupe.length * 2;
+        return dotCount += plusCount;
+        //کلمات عربی ي نقطه دار و ة نقطه دار نیز چک شده و محاسبه میشود
+    };
+    return dotCount
 }
 
 btnElement.addEventListener('click', () => {
     const textareaValue = textarea.value
 
     if (textareaValue) {
-        checkDot(textareaValue)
+        checkDotHandler(textareaValue)
     } else {
         errText.classList.add('active')
     }
